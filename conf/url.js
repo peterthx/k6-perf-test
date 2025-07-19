1715c91f0483318197bd985d77b015b7358e1ffd
peterthx <theerapat.demo@gmail.com>
2025-07-19 21:18:01 +0700

    add script get data pp

--- conf/url.js
+++ conf/url.js
@@ -1,4 +1,7 @@
 export const BASE_URL = {
     URL_HEALTH_CHECK: 'http://localhost:3000/health',
-    URL_GET_ALL_EMP: 'http://localhost:3000/api/employees'
+    URL_GET_ALL_EMP: 'http://localhost:3000/api/employees',
+
+    // get prompt pay
+    URL_GET_PROMPTPAY: 'http://localhost:3000/api/promptpay',
 }
