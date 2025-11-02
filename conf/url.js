45285ac208db55149d40ddeb3ca09ecb7dcb2912
Theerapat.Demo <theerapat.demo@gmail.com>
2025-11-02 16:02:54 +0700

    refactor script test

--- conf/url.js
+++ conf/url.js
@@ -1,4 +1,5 @@
 export const BASE_URL = {
     HEALTH_CHECK: 'http://localhost:3000/health',
-    GET_ALL_EMPLOYEES: 'http://localhost:3000/api/employees'
+    GET_ALL_EMPLOYEES: 'http://localhost:3000/api/employees',
+    REGISTER_CUSTOMER: 'http://srv946485.hstgr.cloud:8080/api/v1/customers'
 }
